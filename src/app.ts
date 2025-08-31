7d2ebba107c017cb591c06a579034a5e5a6c3a12
Kazuki Kanaya Desktop <k.melon0106@gmail.com>
2025-08-31 23:40:06 +0900

    スキーマ更新

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express from "express";
 import type { Application } from "express";
 import cors from "cors";
 import { authRoutes } from "./routes/auth.route";
-import { userRoute } from "./routes/user.rout";
+import { userRoute } from "./routes/user.route";
 
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth", authRoutes);
-app.use("/users", userRoute);
+app.use("/user", userRoute);
 
 app.get("/", (_req, res) => {
   res.send("Welcome to the API!");
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware";
 
 export const userRoute = Router();
-userRoute.get("/", authenticate, (_req, res) => {
+userRoute.get("/profile", authenticate, (_req, res) => {
   res.json({ message: "Hello, user!" });
 });
